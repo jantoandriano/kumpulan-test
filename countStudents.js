1235e9ae5b8dbe01ff9e6112abfb3408b01ffcac
Janto <janto.motulo@sehatq.com>
2022-07-19 16:54:00 +0800

    refactor countStudents

--- countStudents.js
+++ countStudents.js
@@ -1,19 +1,18 @@
 function countStudents(height) {
   // To store a copy of the
   // original array
-  let copy_arr = height.map((item) => item).sort();
-  let n = height.length;
+  let copy_arr = [];
+  let count = 0;
 
   // Copy the elements of the given
   // array to the new array
-  for (let i = 0; i < n; i++) copy_arr[i] = height[i];
-
-  // To store the required count
-  let count = 0;
+  for (let i = 0; i < height.length; i++) {
+    copy_arr[i] = height[i];
+  }
 
   // Sort the original array
   height.sort((a, b) => a - b);
-  for (let i = 0; i < n; i++) {
+  for (let i = 0; i < height.length; i++) {
     // If current element was not
     // at the right position
     if (height[i] != copy_arr[i]) {
@@ -22,3 +21,5 @@ function countStudents(height) {
   }
   return count;
 }
+
+console.log(countStudents([1, 2, 4, 3, 5]));
